4c844c1d6c7bf8f737bfeb39490024bae1632512
kumanaya <daniel.kumanaya@hotmail.com>
2022-01-27 10:32:29 -0800

    (IOS) Setup Splash Screen

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 
 import {
   StatusBar,
 } from 'react-native';
+
+import SplashScreen from  "react-native-splash-screen";
 
 import Routes from "./src/routes";
 
 import Loading from "./src/components/loading";
 
 export default function App() {
+
+  useEffect(() =>{ 
+    SplashScreen.hide()
+  })
+
   return (
     <>
       <StatusBar
